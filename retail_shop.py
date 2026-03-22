d75abae2f072f70660514942fec98821c665e9fc
Gulam N Chabbi <gulamoffical@gmail.com>
2026-03-23 05:14:26 +0530

    Refactor background image setup and imports
    Removed redundant background image setting and cleaned up imports.

--- retail_shop.py
+++ retail_shop.py
@@ -1,26 +1,4 @@
-import streamlit as st
-import base64
 
-def set_bg():
-    with open("static/images/background.jpg", "rb") as f:
-        data = f.read()
-    encoded = base64.b64encode(data).decode()
-
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/jpg;base64,{encoded}");
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-set_bg()
 
 import streamlit as st
 import sqlite3
